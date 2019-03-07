53f2a7a3c0331b5876f8978b536ec291eab32d56
Rodrigo Custodio <jorocuva@gmail.com>
2019-03-07 15:11:01 -0600

    Obj filename change

--- labs/sr5/sr5.c
+++ labs/sr5/sr5.c
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
 	glTranslate(0.0, 0.0, 0.0);
 	glScale(1.0, 1.0, 1.0);
 	glLight(0.0, 0.0, 1.0);
-	glObj("/home/chromz/Downloads/model.obj",
-	      "/home/chromz/Downloads/model.bmp");
+	glObj("/home/chromz/Documents/UVG/Graficas/test.obj",
+	      "/home/chromz/Downloads/HAND_C.bmp");
 	glFinish();
 	return 0;
 }
